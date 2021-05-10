d7a14d18cd0025955f16e8eecbfba118ec11f56e
heyodee <heyodee@gmail.com>
2021-05-10 19:38:21 +0000

    mixed list

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
+print("The SIP socket is: " + my_list[0] + ":"+ str(my_list[1])  )
+print("The last item in the list (state): " + my_list[2] )
