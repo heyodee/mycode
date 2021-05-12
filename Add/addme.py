54e2b4ab9fa1e0bb08312796c84998c4f3e35292
heyodee <heyodee@gmail.com>
2021-05-12 18:31:08 +0000

    Looping techniques

--- Add/addme.py
+++ Add/addme.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3 
 
-num=1
+e=range(10)
 #while num <100000:
-while True:
-    num=num+1
-    print (num)
-    if num > 1000:
-        break
+#while True:
+for range in e:
+    #num=num+1
+    print (range)
+    #if num > 1000:
+    #    break
---
+++ introfor/forloop3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for farm in farms:    
+    print ("On", farm["name"], "we raise:")
+    for animal in farm["agriculture"]:
+        print ("  -", animal)
