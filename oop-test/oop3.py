f1890033aaa9dda6d2511e4fe7cb537d7ea2f08d
heyodee <69757139+heyodee@users.noreply.github.com>
2021-05-14 13:04:33 -0400

    oop

---
+++ oop-test/oop3.py
@@ -0,0 +1,9 @@
+class add:
+    def findsum(self, n1, n2):
+        sum = n1 + n2
+        return sum
+n1 = 100
+n2 = 50
+Obj = add()
+sum =Obj.findsum(n1, n2)
+print("The sum of two numbers is: ", sum)
